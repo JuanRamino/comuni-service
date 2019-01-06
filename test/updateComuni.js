8417f573dc09b07566d0c57067a57ddbae3ec09b
juan ramino <juan.ramino@gmail.com>
2019-01-06 23:31:59 +0100

    Improve test

--- test/updateComuni.js
+++ test/updateComuni.js
@@ -1,16 +1,49 @@
+require('dotenv').config();
 process.env.NODE_ENV = 'test';
 
-const should = require('chai').should();
+const fs = require('fs');
+const should = require('chai').should(); // eslint-disable-line
 const updater = require('../src/comuniUpdater');
 
 
 describe('updateComuni', function() {
   this.timeout(10000);
 
-  it('update without erros', (done) => {
-    updater((err) => {
-      should.not.exist(err);
-      done();
+  const dataPath = `${__dirname}/../data`;
+  const { COMUNI_JSON_FILE } = process.env;
+  const jsonFile = `${dataPath}/${COMUNI_JSON_FILE}`;
+  const jsonFileBk = `${jsonFile}.bk`;
+
+  const jsonExists = fs.existsSync(jsonFile);
+
+  if (jsonExists) {
+    describe('when json data already exists', function() {
+      before(function () {
+        fs.copyFileSync(jsonFile, jsonFileBk);
+      });
+
+      after(function () {
+        fs.renameSync(jsonFileBk, jsonFile);
+      });
+
+      it('update without errors', (done) => {
+        updater((err) => {
+          if (err) {
+            err.should.be.equal(true);
+          }
+          done();
+        });
+      });
     });
-  });
+
+  } else {
+    describe('when there is no json data', function () {
+      it('update without errors', (done) => {
+        updater((err) => {
+          err.should.be.equal(true);
+          done();
+        });
+      });
+    });
+  }
 });
